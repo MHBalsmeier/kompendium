7256c959b68dc094fc22be244ae034e22fb8670d
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2024-01-20 14:09:36 +0100

    sat pressure continued

--- sat_pressure.py
+++ sat_pressure.py
@@ -2,9 +2,27 @@ import numpy as np;
 import matplotlib.pyplot as plt;
 import math;
 
-def sat_pressure_ice(temp_c):
+def saturation_pressure_ice_huang(temp_c):
 	
 	result = math.exp(43.494 - 6545.8/(temp_c + 278.0))/(temp_c + 868.0)**2
+	
+	return result
+
+def saturation_pressure_ice_murphy(temp_k):
+	
+	result = math.exp(9.550426 - 5723.265/temp_k + 3.53068*math.log(temp_k) - 0.00728332*temp_k)
+	
+	return result
+
+def saturation_pressure_ice(temp_c):
+	
+	if temp_c >= -80.0:
+		result = saturation_pressure_ice_huang(temp_c)
+	elif temp_c >= -100.0:
+		huang_weight = (temp_c + 100.0)/20.0
+		result = huang_weight*saturation_pressure_ice_huang(temp_c) + (1.0 - huang_weight)*saturation_pressure_ice_murphy(temp_c + 273.15)
+	else:
+		result = saturation_pressure_ice_murphy(temp_c + 273.15)
 	
 	return result
 
@@ -30,11 +48,11 @@ sat_pressure_water_vector = np.zeros([len(temp_c_water_vector)]);
 for ji in range(len(temp_c_water_vector)):
 	sat_pressure_water_vector[ji] = math.log10(sat_pressure_water(temp_c_water_vector[ji]))
 
-temp_c_ice_vector = np.linspace(-80, 0, 10000);
+temp_c_ice_vector = np.linspace(-120, 0, 10000);
 sat_pressure_ice_vector = np.zeros([len(temp_c_ice_vector)]);
 
 for ji in range(len(temp_c_ice_vector)):
-	sat_pressure_ice_vector[ji] = math.log10(sat_pressure_ice(temp_c_ice_vector[ji]))
+	sat_pressure_ice_vector[ji] = math.log10(saturation_pressure_ice(temp_c_ice_vector[ji]))
 
 fig = plt.figure()
 plt.title("Sättigungsdampfdruck")
