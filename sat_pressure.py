e8193c91888c610b4a170f9093a622d1de57b849
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2024-01-21 01:12:46 +0100

    sat_pressure plot continued

--- sat_pressure.py
+++ sat_pressure.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+t_0 = 273.15
+
 def saturation_pressure_ice_huang(temp_c):
 	
 	result = math.exp(43.494 - 6545.8/(temp_c + 278.0))/(temp_c + 868.0)**2
@@ -42,6 +44,23 @@ def sat_pressure_water(temp_c):
 	
 	return result
 
+def dsat_pressure_water_dT(temperature):
+
+	# This function computes the derivative of the function saturation_pressure_over_water.
+	
+	# calculating the temperature in degrees Celsius
+	temp_c = temperature - t_0
+
+	# these are the limits of this approximation
+	if (temp_c>100.0):
+		dsat_pressure_water_dT = 0.0
+	elif (temp_c<0.0):
+		dsat_pressure_water_dT = 0.0
+	else:
+		dsat_pressure_water_dT = sat_pressure_water(temperature-t_0)*(4924.99/(temp_c + 237.1)**2 - 1.57/(temp_c + 105.0))
+
+	return dsat_pressure_water_dT
+
 temp_c_water_vector = np.linspace(-50, 100, 10000)
 sat_pressure_water_vector = np.zeros([len(temp_c_water_vector)])
 
@@ -66,6 +85,25 @@ plt.ylabel("log10(psat / Pa)")
 plt.grid()
 fig.savefig("figs/sat_pressure.png", dpi = 500)
 
+dsat_pressure_water_vector_dT_ana = np.zeros([len(temp_c_water_vector)])
+dsat_pressure_water_vector_dT_num = np.zeros([len(temp_c_water_vector)])
+
+for ji in range(len(dsat_pressure_water_vector_dT_ana)):
+	temp_c = temp_c_water_vector[ji]
+	dsat_pressure_water_vector_dT_ana[ji] = dsat_pressure_water_dT(temp_c + t_0)
+	dsat_pressure_water_vector_dT_num[ji] = sat_pressure_water(temp_c+0.5) - sat_pressure_water(temp_c-0.5)
+
+fig = plt.figure()
+plt.title("Ableitung Sättigungsdampfdruck")
+plt.plot(temp_c_water_vector, dsat_pressure_water_vector_dT_ana)
+plt.plot(temp_c_water_vector, dsat_pressure_water_vector_dT_num)
+plt.xlim([0*min(temp_c_water_vector), max(temp_c_water_vector)])
+plt.ylim([min(dsat_pressure_water_vector_dT_ana), max(dsat_pressure_water_vector_dT_ana)])
+plt.legend(["über Wasser, analytisch", "über Wasser, numerisch"])
+plt.xlabel(r"Temperatur / $^\circ$C")
+plt.ylabel("dp/dT / Pa")
+plt.grid()
+fig.savefig("figs/dsat_pressure_dT.png", dpi = 500)
 
 
 
