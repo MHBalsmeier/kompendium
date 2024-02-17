5dd2a50e1042560bd9ca1c46eed7060f4725628d
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2024-02-17 11:19:09 +0100

    Monin-Obukhov length plot

--- py/monin_obukhov.py
+++ py/monin_obukhov.py
@@ -1,28 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-z_0 = 0.1
-z_agl = 50.0
-gravity = 9.8
-temp_sfc = 280.0
-u_lowest_layer = 5.0
-karman = 0.4
-temp_atmos = 280.0
-temp_sfc_extrap = temp_atmos + 0.01*z_agl
+# jl = 1000 in ullrich_real
 
-mo_l_vector = np.linspace(1, 100, 1000)
+z_0 = 1.0279899463057518E-002
+z_agl = 291.50790405273438
+gravity = 9.8
+c_d_p = 1005.0
+u_lowest_layer = 3.0568986604327884+20
+karman = 0.4
+temp_sfc = 292.64180557174751
+temp_atmos = 292.58439115255260
+temp_sfc_extrap = temp_atmos + gravity/c_d_p*z_agl
+
+mo_l_vector = np.linspace(0, 3e2, 1000)
 f_vector = np.zeros([len(mo_l_vector)])
+f_vector_simple = np.zeros([len(mo_l_vector)])
 
 for jl in range(len(mo_l_vector)):
 	l = mo_l_vector[jl]
-	a = -gravity*(temp_sfc - temp_sfc_extrap)/(temp_atmos*u_lowest_layer**2*karman)
-	h = np.log(z_agl/z_0) + 4.7*z_agl/l
-	f_vector[jl] = a*l*h**2 - h - np.log(7.0)
+	one_over_a = -temp_atmos*u_lowest_layer**2*karman/(gravity*(temp_sfc - temp_sfc_extrap))
+	f_vector[jl] = one_over_a*(np.log(z_agl/z_0) + 4.7*z_agl/l + np.log(7.0))/(np.log(z_agl/z_0) + 4.7*z_agl/l)**2
+	f_vector_simple[jl] = one_over_a*(np.log(z_agl/z_0) + np.log(7.0))/np.log(z_agl/z_0)**2
 
-plt.title("Monin-Obukhov length minimization function")
+plt.title("Monin-Obukhov length formula (stable conditions)")
+plt.plot(mo_l_vector, mo_l_vector)
 plt.plot(mo_l_vector, f_vector)
+plt.plot(mo_l_vector, f_vector_simple)
 plt.xlabel("Monin-Obukhov length / m")
-plt.xlabel("Minimization function")
+plt.ylabel("Right-hand side / m")
+plt.legend(["l = l", "RHS", "RHS simple"])
+plt.xlim([0, max(mo_l_vector)])
+plt.ylim([0, 1.1*max(f_vector_simple)])
 plt.grid()
 plt.show()
+
+
 
