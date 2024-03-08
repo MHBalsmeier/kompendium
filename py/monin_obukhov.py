376d07fd4166fc2b3e92d6a04c68c5f4c6df1536
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2024-03-09 00:03:01 +0100

    Bugfix in Monin-Obukhov-Laenge

--- py/monin_obukhov.py
+++ py/monin_obukhov.py
@@ -7,19 +7,18 @@ z_0 = 1.0279899463057518E-002
 z_agl = 291.50790405273438
 gravity = 9.8
 c_d_p = 1005.0
-u_lowest_layer = 3.0568986604327884+20
-karman = 0.4
+u_lowest_layer = 3.0568986604327884
 temp_sfc = 292.64180557174751
 temp_atmos = 292.58439115255260
 temp_sfc_extrap = temp_atmos + gravity/c_d_p*z_agl
 
-mo_l_vector = np.linspace(0, 3e2, 1000)
+mo_l_vector = np.linspace(0, 30, 1000)
 f_vector = np.zeros([len(mo_l_vector)])
 f_vector_simple = np.zeros([len(mo_l_vector)])
 
 for jl in range(len(mo_l_vector)):
 	l = mo_l_vector[jl]
-	one_over_a = -temp_atmos*u_lowest_layer**2*karman/(gravity*(temp_sfc - temp_sfc_extrap))
+	one_over_a = -temp_atmos*u_lowest_layer**2/(gravity*(temp_sfc - temp_sfc_extrap))
 	f_vector[jl] = one_over_a*(np.log(z_agl/z_0) + 4.7*z_agl/l + np.log(7.0))/(np.log(z_agl/z_0) + 4.7*z_agl/l)**2
 	f_vector_simple[jl] = one_over_a*(np.log(z_agl/z_0) + np.log(7.0))/np.log(z_agl/z_0)**2
 
