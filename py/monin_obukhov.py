ceec92ef0ea43c33a71429c816240f5a3b8ec095
MHBalsmeier <mhbalsmeier@gmail.com>
2025-05-31 21:24:10 +0200

    wind_profile.png eingefuegt

--- py/monin_obukhov.py
+++ py/monin_obukhov.py
@@ -44,6 +44,53 @@ plt.xlim([0, max(mo_l_vector)])
 plt.ylim([0, 1.1*max(f_vector_simple)])
 plt.grid()
 plt.show()
+plt.close()
+
+# wind profiles
+
+def psi_m(z_eff_over_l):
+
+    # This is a helper function for the correction to the surface momentum flux resistance for non-neutral conditions.
+    
+    # unstable conditions
+    if (np.max(z_eff_over_l)<0.0):
+        x = (1.0 - 16.0*z_eff_over_l)**0.25
+        result = 2.0*np.log((1.0 + x)/2.0) + np.log((1.0 + x**2)/2.0) - 2.0*np.atan(x) + np.pi/2.0
+    # neutral and stable conditions
+    else:
+        result = -4.7*z_eff_over_l
+    
+    return result
+
+height_vector = np.linspace(0.0, 50.0, 1000)
+u_star = 0.5
+karman = 0.4
+z_0 = 0.01
+u_neutral = u_star/karman*(np.log(height_vector/z_0))
+u_stable = u_star/karman*(np.log(height_vector/z_0) - psi_m(height_vector/500.0))
+u_unstable = u_star/karman*(np.log(height_vector/z_0) - psi_m(-height_vector/500.0))
+
+plt.title("Windprofile bei neutraler, stabiler und instabiler Schichtung")
+plt.plot(u_stable[-1]/u_neutral[-1]*u_neutral, height_vector, linestyle = "-", color = "black")
+plt.plot(u_stable, height_vector, linestyle = "dotted", color = "black")
+plt.plot(u_stable[-1]/u_unstable[-1]*u_unstable, height_vector, linestyle = "--", color = "black")
+plt.xlabel("Windgeschwindigkeit / m/s")
+plt.ylabel("Höhe / m")
+plt.legend(["neutral", "stabil", "instabil"])
+plt.xlim([0, np.max(u_stable)])
+plt.ylim([0, np.max(height_vector)])
+plt.grid()
+plt.savefig("../figs/wind_profile.png", dpi = 500)
+plt.close()
+
+
+
+
+
+
+
+
+
 
 
 
