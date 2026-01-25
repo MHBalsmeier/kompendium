3fde0aca8d474f443433ea9f0274bba952cae4ef
MHBalsmeier <mhbalsmeier@gmail.com>
2026-01-25 13:20:39 +0100

    speed_comparison.py weitergefuehrt

--- py/speed_comparison.py
+++ py/speed_comparison.py
@@ -1,14 +1,24 @@
 # This script compares the (theoretical) speeds of WRF and GAME.
 
 timestep_factor_game = 1.61
-timestep_factor_wrf = 7.0
-alpha = 0.5
+n_tau_game = 2
+n_t_game = 1
+timestep_factor_wrf = 8.0
+n_substeps_wrf = timestep_factor_wrf
+n_t_wrf = 3
+n_tracers = 5
+alpha = 0.5 + 0.2*n_tracers
 
-speed_game = timestep_factor_game/2.0*timestep_factor_game/(alpha*timestep_factor_game/2.0 + timestep_factor_game)
-speed_wrf = timestep_factor_wrf/10.0*timestep_factor_wrf/3.0/(alpha*timestep_factor_wrf/10.0 + timestep_factor_wrf/3.0)
+# end of input section
+n_tau_wrf = 1 + int(n_substeps_wrf/2) + n_substeps_wrf
+tau_eff_game = timestep_factor_game/n_tau_game
+tau_eff_wrf= timestep_factor_wrf/n_tau_wrf
 
-print("speed_game: " + str(speed_game))
-print("speed_wrf: " + str(speed_wrf))
+speed_game = tau_eff_game*timestep_factor_game/n_t_game/(alpha*tau_eff_game + timestep_factor_game/n_t_game)
+speed_wrf = tau_eff_wrf*timestep_factor_wrf/n_t_wrf/(alpha*tau_eff_wrf + timestep_factor_wrf/n_t_wrf)
+
+print("speed_game: " + str(round(speed_game, 3)))
+print("speed_wrf: " + str(round(speed_wrf, 3)))
 
 
 
